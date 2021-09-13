12b7c060463b35dfc5211d95986084ae5a4f1fb6
fmy1993 <fmy19930323@outlook.com>
2021-09-13 17:11:06 +0800

    update awagger document

--- application/main.go
+++ application/main.go
@@ -1,27 +1,36 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: fmy1993
+ * @Date: 2021-08-15 15:15:40
+ * @LastEditors: fmy1993
+ * @LastEditTime: 2021-09-13 16:56:17
+ */
 package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/fmy1993/BCexplorer/application/blockchain"
 	_ "github.com/fmy1993/BCexplorer/application/docs"
 	"github.com/fmy1993/BCexplorer/application/pkg/setting"
 	"github.com/fmy1993/BCexplorer/application/routers"
 	"github.com/fmy1993/BCexplorer/application/service"
 	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
-	"time"
 )
 
 func init() {
 	setting.Setup()
 }
 
-// @title 基于区块链技术的房地产交易系统api文档
+// @title 基于区块链技术的农产品溯源系统api文档
 // @version 1.0
-// @description 基于区块链技术的房地产交易系统api文档
-// @contact.name togettoyou
-// @contact.email zoujh99@qq.com
+// @description 基于区块链技术的农产品溯源系统api文档
+// @contact.name fmy
+// @contact.email 1390167880@qq.com
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
